23aa64f634c4b17649f95af591aaba285fc892a3
Evan100354 <evanlong100354@gmail.com>
2024-04-17 02:08:27 +0100

    2 Movement script setup hehe

---
+++ Assets/PlayerMovementAnimation.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerMovementAnimation : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    [SerializeField] public float jumpheight;
+    private Rigidbody2D body;
+    public bool grounded;
+
+    public Animator animator;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+
+        if (body.velocity.magnitude != 0f && grounded && Input.GetKey("a"))
+        {
+            animator.Play("Left");
+        }
+        else if (body.velocity.magnitude != 0f && grounded && Input.GetKey("d"))
+        {
+            animator.Play("Run");
+        }
+        else if (body.velocity.magnitude != 0f && !grounded && Input.GetKey("d"))
+        {
+            animator.Play("Jump");
+        }
+        else if (body.velocity.magnitude != 0f && !grounded && Input.GetKey("a"))
+        {
+            animator.Play("JumpLeft");
+        }
+        else
+        {
+            animator.Play("Idle");
+        }
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpheight);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+}
