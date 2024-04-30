9eabb6ed6075ae2b87d9c970e8f09c52805c55a8
LukeN25 <C22532639@mytudublin.ie>
2024-04-30 11:49:04 +0100

    updating the win point

--- Assets/Scripts/WinPoint.cs
+++ Assets/Scripts/WinPoint.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class WinPoint : MonoBehaviour
 {
+    public GameObject winText;
+    public Rigidbody2D flagBearerRb;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.GetComponent<FlagUnit>())
         {
             Debug.Log("YIPPEEEEEEEEEEE");
+            flagBearerRb.isKinematic = true;
+            winText.SetActive(true);
         }
     }
 }
